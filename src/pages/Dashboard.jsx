549b37f3c04e7e39ae669dd38a4d5b64e8865609
Ashwin Kolhe <89703923+ashvinkolhe@users.noreply.github.com>
2024-11-07 16:54:09 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -93,7 +93,7 @@ const Dashboard = () => {
   const renderTasks = (state) => {
     const filteredTasks = filterTasks(tasks, filter);
     return filteredTasks
-      .filter((task) => task.state === state && task.username == username)
+      .filter((task) => task.state === state)
       .map((task) => (
         <TaskCard
           key={task.id}
