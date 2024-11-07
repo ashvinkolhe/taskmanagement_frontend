579f61b41c5a8602d27ce795809acd950f5102bf
Ashwin Kolhe <89703923+ashvinkolhe@users.noreply.github.com>
2024-11-07 12:56:55 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -50,6 +50,7 @@ const PasswordInput = ({ value, onChange, showPassword, togglePasswordVisibility
 
 const Login = () => {
     const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userDetails, setUserDetails] = useState({ email: '', password: '' });
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState({ email: '', password: '' });
@@ -74,10 +75,7 @@ const Login = () => {
         if (!validateFields()) return;
 
         try {
-            const { data } = await axios.post('https://taskmanagement-backend-9ztm.onrender.com/api/users/login', {
-                email: userDetails.email,
-                password: userDetails.password
-            });
+            const { data } = await axios.post('https://taskmanagement-backend-9ztm.onrender.com/api/users/login', userDetails);
 
             if (data?.status === "success") {
                 localStorage.setItem('authToken', data.token);
@@ -85,6 +83,7 @@ const Login = () => {
                 localStorage.setItem('id', data.user.id);
 
                 toast.success('Login Successful');
+                setIsAuthenticated(true);
                 navigate('/dashboard');
             } else {
                 toast.error('Invalid Credentials');
